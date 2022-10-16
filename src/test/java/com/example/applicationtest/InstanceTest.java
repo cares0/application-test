04d142f89c3629e9bd980c1a5ece8eac01bf503e
cares0 <cares00000@gmail.com>
2022-10-16 22:55:27 +0900

    Test Instance

---
+++ src/test/java/com/example/applicationtest/InstanceTest.java
@@ -0,0 +1,34 @@
+package com.example.applicationtest;
+
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class InstanceTest {
+    
+    private int value = 0;
+    
+    @Test
+    void method1() {
+        value++;
+        System.out.println("value = " + value); // value = 1
+        System.out.println("this = " + this);
+    }
+
+    @Test
+    void method2() {
+        value++;
+        System.out.println("value = " + value); // value = 1
+        System.out.println("this = " + this);
+    }
+
+    @BeforeAll
+    public void beforeAll() {
+        System.out.println("before value = " + value);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.out.println("after value = " + value);
+    }
+
+}
