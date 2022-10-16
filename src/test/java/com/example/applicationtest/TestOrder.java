461ce5eec383f7b31ffeb4e387bbed005882a6bc
cares0 <cares00000@gmail.com>
2022-10-16 22:55:41 +0900

    Test Order 지정

---
+++ src/test/java/com/example/applicationtest/TestOrder.java
@@ -0,0 +1,38 @@
+package com.example.applicationtest;
+
+import org.junit.jupiter.api.*;
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestOrder {
+
+    private int value = 0;
+
+    @Test
+    @Order(3)
+    void method1() {
+        value++;
+        System.out.println("test method 1");
+        System.out.println("value = " + value);
+        System.out.println("this = " + this);
+    }
+
+    @Test
+    @Order(1)
+    void method2() {
+        value++;
+        System.out.println("test method 2");
+        System.out.println("value = " + value);
+        System.out.println("this = " + this);
+    }
+
+    @Test
+    @Order(2)
+    void method3() {
+        value++;
+        System.out.println("test method 3");
+        System.out.println("value = " + value);
+        System.out.println("this = " + this);
+    }
+
+}
